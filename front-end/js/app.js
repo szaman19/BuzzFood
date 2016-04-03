a34076f9091fafcb657286a9dc17ce367bcd1484
Shehtab Zaman <shehtabzaman@172-30-17-250.DYNAPOOL.NYU.EDU>
2016-04-03 00:14:51 -0400

    server updates

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,31 +1,27 @@
 (function(){
-var app = angular.module('store',[]);
+var app = angular.module('food',[]);
 
-app.controller('StoreController',['$http',function($http){
-  var store =this;
-  store.products = [];
-  $http.get('info.json').success(function(data){
-    store.products = data;
-  });
+app.controller('SearchController',['$http',function($http){
+  }
+]);
+
+app.controller('formCtrl',['$http','$scope',function($http,$scope){
+  this.query = '';
+  // this.submit = function(){
+  //   this.queryText = queryText;
+  // };
+  this.submit = function(){
+    $http({
+      url : "http://127.0.0.1:5000/query",
+      method: "GET",
+      params :{'query':this.query}
+    }).success(function(data){
+      console.log(data);
+    });
+    console.log(this.query);
+  }
+  this.returnQuery = function(){
+    return this.queryText;
+  }
 }]);
-
-app.controller('ReviewController',function(){
-  this.review = {};
-  this.addReview = function(product){
-    product.reviews.push(this.review);
-    this.review = {};
-  };
-});
-
-app.controller('TabController',function(){
-  this.tab = 0;
-  this.setTab = function(num){
-    this.tab = num;
-  };
-
-  this.isSet = function(num){
-    return this.tab === num;
-  };
-});
-
 })();
