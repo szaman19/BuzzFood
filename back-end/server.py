86c03a0c2d2c4505584f0703cf05c22b4b2158d8
Shehtab Zaman <shehtabzaman@172-30-17-250.DYNAPOOL.NYU.EDU>
2016-04-03 06:28:11 -0400

    radical changes be coming

--- back-end/server.py
+++ back-end/server.py
@@ -39,12 +39,43 @@ def returnListOfImages():
     # returnArray.append(request.args['query'])
     # return jsonify(result=returnArray)
 @app.route('/imageTags',methods=['GET'])
+
 def getImageTag():
-    imageUrls = request.args['urls']
-    print(imageUrls) #String representation of list of image urls
+    imageUrlString = request.args['urls']
+    imageUrlString = imageUrlString.strip('[').strip(']').replace('"','')
+    # print(imageUrlString)
+
+    imageUrlList = imageUrlString.split(',')
+    tagDict = {}
+    for imageURL in imageUrlList:
+        header = {'Authorization' : "Bearer 9PLrJMMB6sl4qoSK8JPf8gy9mRVfvL"}
+        data = {'model' : "food-items-v0.1", 'url' : imageURL}
+        r = requests.get("https://api.clarifai.com/v1/tag/",headers = header,params=data)
+        JSONTags = r.json()
+        print(JSONTags)
+
+        try:
+            tagData = JSONTags['results'][0]['result']['tag']
+            tagList = zip(tagData['classes'],tagData['probs'])
+            for tag in tagList:
+                if tag[1] > .8:
+                    tagName = tag[0].encode()
+                    if not tagName in tagDict:
+                        tagDict[tagName] = 0
+                    tagDict[tagName] += 1
+        except Exception as e:
+            print(e)
+            print(JSONTags)
+    print(tagDict)
+    # return tagDict
 
 
-    print(type(imageUrls))
+    # print(imageUrlList) #String representation of list of image urls
+    print(imageUrlList) #String representation of list of image urls
+
+
+
+    # print(type(imageUrls))
     return "Working"
 
 
