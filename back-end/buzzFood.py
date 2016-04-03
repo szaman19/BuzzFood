b4aecd5cba0f110ed9f939d5501ee0211fa6ee25
Harvey Chen <harvc015@gmail.com>
2016-04-03 03:07:57 -0400

    Updated some functions

--- back-end/buzzFood.py
+++ back-end/buzzFood.py
@@ -20,39 +20,40 @@ def getImageUrls(query):
 	jsonDump = json.dumps(r.json())
 	searchResults = json.loads(jsonDump)
 
-	urlList = []
+	imageUrlDict = {}
 	for result in searchResults["items"]:
-	##	urlList.append(result['image']['thumbnailLink'])
-		urlList.append(result['link'].encode('utf-8'))
+		## Key is url of thumbnail, value is link to full image
+		imageUrlDict[result['image']['thumbnailLink'].encode('utf-8')] = result['link'].encode('utf-8')
 
-	print json.dumps(searchResults, indent=4, sort_keys=True)
-	print urlList
-	return urlList
+	return imageUrlDict
 
-def getTags(imageURLs):
-	tagsDict = {}
-	for imageURL in imageURLs:
+def getTags(imageUrlDict):
+	imageUrlValues = imageUrlDict.values()
+	tagDict = {}
+
+	for imageURL in imageUrlValues:
 		header = {'Authorization' : "Bearer 9PLrJMMB6sl4qoSK8JPf8gy9mRVfvL"}
 		data = {'model' : "food-items-v0.1", 'url' : imageURL}
 
 		r = requests.get("https://api.clarifai.com/v1/tag/",headers = header,params=data)
 		JSONTags = r.json()
 
-		tagsData = JSONTags['results'][0]['result']['tag']
-		tagList = zip(tagsData['classes'],tagsData['probs'])
+		tagData = JSONTags['results'][0]['result']['tag']
+		tagList = zip(tagData['classes'],tagData['probs'])
+
 		for tag in tagList:
 			if tag[1] > .8:
 				tagName = tag[0].encode()
-				if not tagsDict.has_key(tagName):
-					tagsDict[tagName] = 0
-				tagsDict[tagName] += 1
+				if not tagDict.has_key(tagName):
+					tagDict[tagName] = 0
+				tagDict[tagName] += 1
 
-	return tagsDict
+	return tagDict
 
 def fourSearch(tagDict):
 	clientID = 'JD5WEQCZLAOXXPVBYZV1TOUONBMWCUXNHCJALUSQN5YTUHKA'
 	clientSecret = '1QW0UZYHRAVPJGCWA04JSJGHJ0RWEPW3XG3AF3GYJFUTYQRU'
-	client = foursquare.Foursquare(client_id = clientID, client_secret= clientSecret)
+	client = foursquare.Foursquare(client_id = clientID, client_secret = clientSecret)
 
 	restaurantDict = {}
 	tagKeys = tagDict.keys()
@@ -68,17 +69,17 @@ def fourSearch(tagDict):
 
 	return restaurantDict
 
-print fourSearch({"Pizza" : 2, "Taco" : 1})
-
 def restaurantRank(restaurantDict):
-	return 0
+	matchValue = sorted(set(restaurantDict.values()))
+	return matchValue
 
 def main():
-##	imageURLs = getImageUrls("food")
-	imageURLs = ['http://www.healthyfoodteam.com/wp-content/uploads/2014/02/Which-Food-You-Should-Never-Mix.jpg', 'http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2014/08/20/63/food-paradise-106-burritos-ss-003', 'http://cdn.playbuzz.com/cdn/39f85817-18ad-4321-9908-03b342872b87/7838a2f8-fb2d-48e8-abc9-f7db942d3ede.jpg', 'http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2015/03/24/bizarre-foods-delicious-destinations-112-hong-kong-ss/bizarre-foods-delicious-destinations-ss-038-hong-kong-typhoon-shelter-crab.jpg.rend.tccom.1280.960.jpeg', 'https://i.ytimg.com/vi/SeTpJNRnG6w/maxresdefault.jpg', 'http://d.fastcompany.net/multisite_files/fastcompany/imagecache/1280/poster/2015/01/3041647-poster-p-1-most-innovative-companies-2015-next-sectors-food.jpg', 'https://pixabay.com/static/uploads/photo/2015/07/30/18/23/burger-868145_960_720.jpg', 'https://i.ytimg.com/vi/JWsob3MZUQ4/maxresdefault.jpg', 'http://media4.popsugar-assets.com/files/2013/07/02/953/n/1922195/426ebedb72d4cbbd_taste-the-states-cover.xxxlarge/i/US-State-Foods.jpg', 'https://i.ytimg.com/vi/uDnd_C8Hkp8/maxresdefault.jpg']
-	tags = getTags(imageURLs)
+##	imageUrlDict = getImageUrls("food")
+	imageUrlDict = {'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq4RxycF0BLa1xbBh3MhKhl7uqN-YdbGu8uHoCACyn3yUn3qKJr0C-2RYz': 'http://media4.popsugar-assets.com/files/2013/07/02/953/n/1922195/426ebedb72d4cbbd_taste-the-states-cover.xxxlarge/i/US-State-Foods.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9_x4IhsaCSKSsnjrtfX920IpOsU2fGktKUGICSf0Bio13WvCAITy2NOgx': 'http://www.healthyfoodteam.com/wp-content/uploads/2014/02/Which-Food-You-Should-Never-Mix.jpg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTAKAkjZvDkDTJc3Q9iqfGU6x7ywv6oeD8hlpvzE0I6glrXvcU7-eDcebpu': 'http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2014/08/20/63/food-paradise-106-burritos-ss-003', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWJWbrqRHY4_KJVdYVxd4PvHzRqFjZInhDy1NDRcslhCM3QLObWIBb4fz7': 'http://travelchannel.sndimg.com/content/dam/images/travel/fullset/2015/03/24/bizarre-foods-delicious-destinations-112-hong-kong-ss/bizarre-foods-delicious-destinations-ss-038-hong-kong-typhoon-shelter-crab.jpg.rend.tccom.1280.960.jpeg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRiceNRzk1Y9uwJLg2s3gAGj_5s5HVhlmaYSXb7lOEvQv3SIdlBGeHYQGje': 'https://i.ytimg.com/vi/uDnd_C8Hkp8/maxresdefault.jpg', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAnOj2m37X-mEqppiSQz1osMIVj1O5Ar9oBNIR64T0bwWjhHM3aAjMipHi': 'http://d.fastcompany.net/multisite_files/fastcompany/imagecache/1280/poster/2015/01/3041647-poster-p-1-most-innovative-companies-2015-next-sectors-food.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHhW_JNH2I6A1XmeDv29KhyiYCjlIbZZqUvmMVoDsDcvT1E05FgXLcqBs': 'https://i.ytimg.com/vi/SeTpJNRnG6w/maxresdefault.jpg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTcrqYznCNDlIoi3r6VzpYM_F9E-iYkbMPYdr3NtCQ0nfAdV5g-Khs3hlM': 'http://cdn.playbuzz.com/cdn/39f85817-18ad-4321-9908-03b342872b87/7838a2f8-fb2d-48e8-abc9-f7db942d3ede.jpg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT_bTJKkp6AzOw1DYp_ab0LqgczobdHuH3jZky_slSiUtYzFf2qmlpetmg': 'https://pixabay.com/static/uploads/photo/2015/07/30/18/23/burger-868145_960_720.jpg', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSJkwTZV-vDHcrJkwKFdcc20iqbUjKh-NwW3lMJN5Jmko3YUJ0j7mhOAfq4': 'https://i.ytimg.com/vi/JWsob3MZUQ4/maxresdefault.jpg'}
+	tags = getTags(imageUrlDict)
 	print tags
 	search = fourSearch(tags)
 	print json.dumps(search, indent=4, sort_keys=True)
+	print restaurantRank(search)
 
 main()
